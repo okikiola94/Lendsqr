3f32dc7715d2e829e3fb7ce45d12f15795fb5545
okikiola94 <71730677+okikiola94@users.noreply.github.com>
2023-01-11 15:26:40 +0100

    fix:popup menu action to navigate to user details

--- src/components/PopAction/index.tsx
+++ src/components/PopAction/index.tsx
@@ -1,37 +1,49 @@
 import { MouseEventHandler } from "react";
+import { BsEye } from "react-icons/bs";
+import { BiUserCheck, BiUserX } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 
 //STYLES
 import styles from "./PopUp.module.scss";
 
 interface User {
-    id: string;
+  id: string;
 }
 
+const DeletePopup = (props: {
+  exit: MouseEventHandler<HTMLAnchorElement> | undefined;
+}) => {
+  const navigate = useNavigate();
 
-const DeletePopup = (props: { exit: MouseEventHandler<HTMLAnchorElement> | undefined; }) => {
-const navigate=useNavigate();
-
-    return (
-        <div>
-            <div className={styles.PopUp} id="mySidebar">
-                <a href="javascript:void(0)" className="closebtn" onClick={props.exit}>
-                    &times;
-                </a>
-                <div className="KEBAB-menu">
-                    <div className="vIWV-USER-side">
-                        <li onClick={() => navigate("/userdetails")}>View Details </li>
-                    </div>
-                    <div className="Edit-user-side">
-                        <li onClick={() => navigate("/admin/userlist")}>Blacklist User </li>
-                    </div>
-                    <div className="Archive-User-side">
-                        <li onClick={() => navigate("/admin/archive")}>Activate User</li>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={styles.PopUp} id="mySidebar">
+        <a href="javascript:void(0)" className={styles.PopUpClosebtn} onClick={props.exit}>
+          &times;
+        </a>
+        <div className={styles.PopUPKebabMmenu}>
+          <li
+            onClick={() => navigate("/userdetails")}
+            className={styles.PopUplistItem}
+          ><BsEye/>
+            View Details{" "}
+          </li>
+          <li
+            onClick={() => navigate("/admin/userlist")}
+            className={styles.PopUplistItem}
+          ><BiUserX/>
+            Blacklist User{" "}
+          </li>
+          <li
+            onClick={() => navigate("/admin/archive")}
+            className={styles.PopUplistItem}
+          ><BiUserCheck/>
+            Activate User
+          </li>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DeletePopup;
